4aa479ea03edaf3878345a398e254ce75588e201
hiro627 <80045736+hiro627@users.noreply.github.com>
2021-06-18 01:26:28 +0900

    Update TodoListView.js

--- src/view/TodoListView.js
+++ src/view/TodoListView.js
@@ -2,13 +2,8 @@ import { element } from "./html-util.js";
 import { TodoItemView } from "./TodoItemView.js";
 
 export class TodoListView {
-    /**
-     * `todoItems`に対応するTodoリストのHTML要素を作成して返す
-     * @param {TodoItemModel[]} todoItems TodoItemModelの配列
-     * @param {function({id:string, completed: boolean})} onUpdateTodo チェックボックスの更新イベントリスナー
-     * @param {function({id:string})} onDeleteTodo 削除ボタンのクリックイベントリスナー
-     * @returns {Element} TodoItemModelの配列に対応したリストのHTML要素
-     */
+    
+     // `todoItems`に対応するTodoリストのHTML要素を作成して返す
     createElement(todoItems, { onUpdateTodo, onDeleteTodo }) {
         const todoListElement = element`<ul />`;
         // 各TodoItemモデルに対応したHTML要素を作成し、リスト要素へ追加する
