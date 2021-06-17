a40d10215907b492e405136b1227a01ba00d3314
hiro627 <80045736+hiro627@users.noreply.github.com>
2021-06-18 01:30:39 +0900

    Update TodoListModel.js

--- src/model/TodoListModel.js
+++ src/model/TodoListModel.js
@@ -1,57 +1,43 @@
 import { EventEmitter } from "../EventEmitter.js";
 
 export class TodoListModel extends EventEmitter {
-    /**
-     * @param {TodoItemModel[]} [items] 初期アイテム一覧（デフォルトは空の配列）
-     */
     constructor(items = []) {
         super();
         this.items = items;
     }
 
-    /**
-     * TodoItemの合計個数を返す
-     * @returns {number}
-     */
+
+     // TodoItemの合計個数を返す
     getTotalCount() {
         return this.items.length;
     }
 
-    /**
-     * 表示できるTodoItemの配列を返す
-     * @returns {TodoItemModel[]}
-     */
+   
+     // 表示できるTodoItemの配列を返す
     getTodoItems() {
         return this.items;
     }
 
-    /**
-     * TodoListの状態が更新されたときに呼び出されるリスナー関数を登録する
-     * @param {Function} listener
-     */
+
+     // TodoListの状態が更新されたときに呼び出されるリスナー関数を登録する
     onChange(listener) {
         this.addEventListener("change", listener);
     }
 
-    /**
-     * 状態が変更されたときに呼ぶ。登録済みのリスナー関数を呼び出す
-     */
+   
+     // 状態が変更されたときに呼ぶ。登録済みのリスナー関数を呼び出す
     emitChange() {
         this.emit("change");
     }
 
-    /**
-     * TodoItemを追加する
-     * @param {TodoItemModel} todoItem
-     */
+
+     // TodoItemを追加する
     addTodo(todoItem) {
         this.items.push(todoItem);
         this.emitChange();
     }
-    /**
-     * 指定したidのTodoItemを削除する
-     * @param {{ id: number }}
-     */
+
+     // 指定したidのTodoItemを削除する
     deleteTodo({ id }) {
         // `id`に一致しないTodoItemだけを残すことで、`id`に一致するTodoItemを削除する
         this.items = this.items.filter(todo => {
